42ed9c98c502ab573458482c80597845bafd6587
LauraCortes835 <laurajimenacortessoto@gmail.com>
2020-09-21 21:32:08 -0500

    Laura Cortes

--- js/script.js
+++ js/script.js
@@ -1,21 +1,88 @@
-function ispaAmong() {
-    let impostor= true; /* True- verdadero osea que si eres el impostor */
-
-    /* Si el impostor NO es true/verdadero, osea es false....un tripulante mas */
+function impostorAmong1() {
+    let impostor= true; 
     if (impostor == !true) {
         alert("Eres un tripulante, ojala no te funen")
-    } else { /* Si es true entonces es un impostor */
-        alert("Eres un impostor, cuidado que nadie te descubra")
+    } else { 
+        alert("IMPOSTOR: Verde, nadie se lo espera de ti")
     }
 }
-function ispaAmong2() {
-    let impostor= true; /* True- verdadero osea que si eres el impostor */
-
-    /* Si el impostor NO es true/verdadero, osea es false....un tripulante mas */
+function tripulanteAmong1() {
+    let impostor= true;
     if (impostor == !true) {
         alert("Eres un impostor, cuidado que nadie te descubra")
         
-    } else { /* Si es true entonces es un impostor */
+    } else { 
+        alert("TRIPULANTE: Eres el amarillo, todos sospechan de ti")
+    }
+}
+function tripulanteAmong2() {
+    let impostor= true;
+    if (impostor == !true) {
+        alert("Eres un impostor, ojito")
+        
+    } else { 
+        alert("TRIPULANTE: Cafe, nadie sospecha de ti, ojala sobrevivas")
+    }
+}
+function tripulanteAmong3() {
+    let impostor= true;
+    if (impostor == !true) {
+        alert("Eres un impostor, cuidado que nadie te descubra")
+        
+    } else { 
+        alert("TRIPULANTE: Rojo, intenta hacer las misiones eres el que no hace nada")
+    }
+}
+function tripulanteAmong4() {
+    let impostor= true;
+    if (impostor == !true) {
+        alert("Eres un impostor, cuidado que nadie te descubra")
+        
+    } else { 
+        alert("TRIPULANTE: Eres el negro, te funan por racistas :c")
+    }
+}
+function tripulanteAmong5() {
+    let impostor= true;
+    if (impostor == !true) {
+        alert("Eres un impostor, cuidado que nadie te descubra")
+        
+    } else { 
+        alert("TRIPULANTE: Rosa nadie sospecha de ti,  suerte ")
+    }
+}
+function tripulanteAmong6() {
+    let impostor= true;
+    if (impostor == !true) {
+        alert("Eres un impostor, cuidado que nadie te descubra")
+        
+    } else { 
+        alert("TRIPULANTE: Azul, eres el unico que hace todas la tareas rapido, mas gente como tu ")
+    }
+}
+function tripulanteAmong7() {
+    let impostor= true;
+    if (impostor == !true) {
+        alert("Eres un impostor, cuidado que nadie te descubra")
+        
+    } else { 
+        alert("TRIPULANTE: Blanco, eres el que persigue a todos, por eso te funan ")
+    }
+}
+function tripulanteAmong8() {
+    let impostor= true;
+    if (impostor == !true) {
+        alert("Eres un impostor, cuidado que nadie te descubra")
+        
+    } else { 
+        alert("TRIPULANTE: Naranja, eres muy sospechoso a veces, te pueden funas por eso, cuidado ")
+    }
+}
+function impostorAmong2() {
+    let impostor= true; 
+    if (impostor == !true) {
         alert("Eres un tripulante, ojala no te funen")
+    } else { 
+        alert("IMPOSTOR: Celeste, ten cuidado cuando entres a una alcantarilla")
     }
 }
